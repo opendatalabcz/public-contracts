e47b5109c33f216341e542d242301ef692465c3f
Zdenek Balak <balakzde@fit.cvut.cz>
2016-03-27 06:18:55 +0200

    Git rename bug fix

--- src/test/java/TestForDevelopers.java
+++ src/test/java/TestForDevelopers.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import service.DatabaseService;
+import service.DatabaseServiceRenamed;
 import service.ISVZCrawlerService;
 import service.ISVZService;
 
@@ -19,7 +19,7 @@ import java.util.List;
 public class TestForDevelopers {
 
     @Autowired
-    private DatabaseService databaseService;
+    private DatabaseServiceRenamed databaseService;
     @Autowired
     private ISVZService isvzService;
     @Autowired
